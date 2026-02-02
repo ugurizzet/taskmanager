fa7fa887afdaac3f9a82a46266fbb05ef5ce15d8
izzetonal <izzetonal46@gmail.com>
2026-02-02 13:17:48 +0100

    ci test 3

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 const dotenv = require('dotenv');
 require('dotenv').config();
 
+const dbPath = process.env.NODE_ENV === 'test' 
+    ? ':memory:' 
+    : (process.env.DB_PATH || './taskmanager.db');
+
 const db = new sqlite3.Database(process.env.DB_PATH , (err) => {
     if (err) {
         console.error('Database connection error:', err.message);
